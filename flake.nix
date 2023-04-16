34e931a55e40915476e2cc9824319e48f907bf98
Christian Kemper <christian.kemper@me.com>
2023-04-16 19:55:11 +0100

    nix flake build

--- flake.nix
+++ flake.nix
@@ -1,21 +1,47 @@
 {
-  description = "devshell";
+  description = "A simple terminal multiplexer written in Rust";
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, flake-utils, devshell, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
+  outputs = { self, flake-utils, devshell, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-
             overlays = [ devshell.overlays.default ];
           };
+          cargoToml = builtins.fromTOML (builtins.readFile (builtins.toString ./. + "/Cargo.toml"));
+          rmux = pkgs.rustPlatform.buildRustPackage
+            {
+              pname = cargoToml.package.name;
+              version = cargoToml.package.version;
+
+              src = ./.;
+
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+
+              checkType = "debug";
+
+              meta = with pkgs.lib; {
+                description = cargoToml.package.description;
+                homepage = cargoToml.package.homepage;
+                license = licenses.unlicense;
+              };
+            };
         in
-        pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-        };
-    });
+        {
+          defaultPackage = rmux;
+          devShell =
+            pkgs.devshell.mkShell {
+              packages = [ pkgs.cargo pkgs.rustc rmux ];
+              imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+            };
+        }
+      );
 }
