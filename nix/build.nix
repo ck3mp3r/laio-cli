47b9f7f26b6de7413c742fe6296842b6f74d7777
Christian Kemper <christian.kemper@me.com>
2023-11-18 21:59:03 +0000

    simplifying and removing static flags

--- nix/build.nix
+++ nix/build.nix
@@ -14,11 +14,9 @@ in
 
   nativeBuildInputs = with pkgs;[
     installShellFiles
-    # stdenv.cc
   ] ++ lib.optionals stdenv.isLinux [
     patchelf
   ] ++ lib.optionals stdenv.isDarwin [
-    # darwin.libtapi
   ];
 
   buildInputs = with pkgs; [
@@ -26,60 +24,9 @@ in
     # libiconv
   ];
 
-  # configurePhase = ''
-  #   rustc --version
-  # '';
-
   src = ../.;
 
   cargoLock.lockFile = ../Cargo.lock;
-
-  CARGO_BUILD_TARGET = buildTarget;
-  RUSTFLAGS =
-    if stdenv.isLinux then
-      [
-        "-C link-arg=-static"
-      ]
-    else
-      [
-        # "-C link-arg=-static"
-        # "-C target-feature=+crt-static"
-      ];
-
-  # CARGO_BUILD_RUSTFLAGS =
-  #   if stdenv.isLinux then
-  #     [ "-C linker=rust-lld" ]
-  #   else
-  #     [
-  #       # "-C link-arg=-static"
-  #       # "-C target-feature=+crt-static"
-  #     ];
-  # "CARGO_TARGET_${shout buildTarget}_LINKER" = "${pkgs.stdenv.cc.targetPrefix}ld";
-  # NIX_LDFLAGS = lib.optionalString stdenv.isDarwin "-framework System";
-
-  # CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER =
-  #   let
-  #     inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
-  #   in
-  #   "${cc}/bin/${cc.targetPrefix}cc";
-
-  # CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER =
-  #   let
-  #     inherit (pkgs.pkgsCross.musl64.stdenv) cc;
-  #   in
-  #   "${cc}/bin/${cc.targetPrefix}cc";
-
-  # CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER =
-  #   let
-  #     inherit (pkgs.pkgsCross.aarch64-darwin.stdenv) cc;
-  #   in
-  #   "${cc}/bin/${cc.targetPrefix}cc";
-
-  # CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER =
-  #   let
-  #     inherit (pkgs.stdenv) cc;
-  #   in
-  #   "${cc}/bin/${cc.targetPrefix}cc";
 
   meta = with pkgs.lib;
     {
