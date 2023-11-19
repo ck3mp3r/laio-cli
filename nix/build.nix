78ba643f9033871b56421d44689b8e4ea8a8fc5b
Christian Kemper <christian.kemper@me.com>
2023-11-19 20:23:15 +0000

    --wip-- [skip ci]

--- nix/build.nix
+++ nix/build.nix
@@ -12,39 +12,12 @@ in
   name = cargoToml.package.name;
   version = cargoToml.package.version;
 
-  nativeBuildInputs = with pkgs;[
-    # autoPatchelfHook
-    # patchelf
-    # python3
-    # installShellFiles
-    # autoPatchelfHook
-    # python3
-  ] ++ lib.optionals stdenv.isLinux [
-    # patchelf
-  ] ++ lib.optionals stdenv.isDarwin [
-  ];
-
-  buildInputs = with pkgs; [
-  ] ++ lib.optionals stdenv.isLinux [
-    # stdenv.cc.cc.lib
-  ] ++ lib.optionals stdenv.isDarwin [
-    # libiconv
-  ];
-
   src = ../.;
-
-  RUSTFLAGS = [
-
-  ] ++ pkgs.lib.optionals stdenv.isLinux [
-    # "-C target-feature=+crt-static"
-  ];
 
   cargoLock.lockFile = ../Cargo.lock;
 
   installPhase = ''
-    # runHook preInstall
     install -m755 -D target/${config}/release/rmx $out/bin/rmx
-    # runHook postInstall
   '';
 
   meta = with pkgs.lib;
