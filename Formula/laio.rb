11e4477078f59483ba5d3f2dfbe7ded4577837a9
Christian <christian.kemper@me.com>
2024-09-15 18:06:50 +0000

    Releasing v0.10.6

--- Formula/laio.rb
+++ Formula/laio.rb
@@ -1,27 +1,27 @@
 class Laio < Formula
   desc "laio - a simple, flexbox-inspired, layout & session manager for tmux."
   homepage "https://laio.sh"
-  version "0.10.5"
+  version "0.10.6"
 
   depends_on "tmux"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.5/laio-0.10.5-x86_64-darwin.tgz"
-      sha256 "859eb70dfc95fb797116aa6f18d2575fc4af6097354f9f5bddce128f1c4bbfb8"
+      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.6/laio-0.10.6-x86_64-darwin.tgz"
+      sha256 "a9377a29b2597b823775ee5b54a19bcac9658c82f9347fe55a1e586f7c31f153"
     elsif Hardware::CPU.arm?
-      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.5/laio-0.10.5-aarch64-darwin.tgz"
-      sha256 "b6a410764bda5e4a58f1132ad3c31b7a100dfa3dec34dcfb9f74e70294ad133f"
+      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.6/laio-0.10.6-aarch64-darwin.tgz"
+      sha256 "c757c39a8e347b03537005e2a8907c3feb9b5f15fc7397e5bd91524071c18b68"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.5/laio-0.10.5-x86_64-linux.tgz"
-      sha256 "f7a87559b5b8b7c1560f479e931be8805100741e2bf4ecfb2fc3c6430ee8f1e0"
+      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.6/laio-0.10.6-x86_64-linux.tgz"
+      sha256 "76a44f23e7937fd7c4686a5f9cc46d91bf3f1ac3ad635111ec2902699caf3ff4"
     elsif Hardware::CPU.arm?
-      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.5/laio-0.10.5-aarch64-linux.tgz"
-      sha256 "0f589f3356e4b276e179f6ec12a19239f94ce8f1525b6af613e08dedf932b643"
+      url "https://github.com/ck3mp3r/laio-cli/releases/download/v0.10.6/laio-0.10.6-aarch64-linux.tgz"
+      sha256 "e23552f855b735188404860a56d6ada6dd610947a72a24e5ea356a2ce7c15cde"
     end
   end
 
