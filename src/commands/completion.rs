edfb260656574968d1326e0b8b25a438f51831bc
Christian Kemper <christian.kemper@me.com>
2024-11-08 18:32:16 +0000

    adding nushell completion (#131)

--- src/commands/completion.rs
+++ src/commands/completion.rs
@@ -1,24 +1,53 @@
 use anyhow::Result;
-use clap::{Args, CommandFactory};
+use clap::{Args, CommandFactory, ValueEnum};
 use clap_complete::{generate, Shell};
+use clap_complete_nushell::Nushell;
 use std::io;
 
 use crate::commands::cli::Cli as RootCli;
 
-/// Display the completion file for a given shell
 #[derive(Args, Debug)]
 #[command()]
 pub struct Cli {
     #[arg(value_enum)]
-    shell: Shell,
+    shell: ShellWrapper,
+}
+
+#[derive(Debug, Clone)]
+enum ShellWrapper {
+    Builtin(Shell),
+    Nushell,
+}
+
+impl ValueEnum for ShellWrapper {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[
+            Self::Builtin(Shell::Bash),
+            Self::Builtin(Shell::Elvish),
+            Self::Builtin(Shell::Fish),
+            Self::Builtin(Shell::PowerShell),
+            Self::Builtin(Shell::Zsh),
+            Self::Nushell,
+        ]
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            Self::Builtin(shell) => shell.to_possible_value(),
+            Self::Nushell => Some(clap::builder::PossibleValue::new("nushell")),
+        }
+    }
 }
 
 impl Cli {
     pub fn run(&self) -> Result<()> {
         let mut cmd = RootCli::command();
         let bin_name = cmd.get_name().to_string();
-        generate(self.shell, &mut cmd, bin_name, &mut io::stdout());
 
+        match &self.shell {
+            ShellWrapper::Nushell => generate(Nushell, &mut cmd, bin_name, &mut io::stdout()),
+            ShellWrapper::Builtin(shell) => generate(*shell, &mut cmd, bin_name, &mut io::stdout()),
+        }
         Ok(())
     }
 }
