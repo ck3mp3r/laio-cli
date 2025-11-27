5f79ac82dec19df76d673eb5afb7b36f4c2c13c6
Christian Kemper <christian.kemper@me.com>
2025-11-27 12:21:20 +0000

    Add pre-commit hook to fix trailing whitespace
    - Implement fix-whitespace git hook for staged files
    - Integrate hook using gnused and shell script in pre-commit stage

--- nix/devenv/developer.nix
+++ nix/devenv/developer.nix
@@ -19,10 +19,29 @@
     coverage.exec = "cargo tarpaulin --out Html";
   };
 
-  git-hooks.hooks.pre-push = {
-    enable = true;
-    entry = "cargo test -- --include-ignored";
-    stages = ["pre-push"];
+  git-hooks.hooks = {
+    fix-whitespace = {
+      enable = true;
+      name = "Fix trailing whitespace";
+      entry = "${pkgs.writeShellScript "fix-whitespace" ''
+        # Fix trailing whitespace in staged files
+        git diff --cached --name-only --diff-filter=ACM | while read file; do
+          if [ -f "$file" ]; then
+            ${pkgs.gnused}/bin/sed -i 's/[[:space:]]*$//' "$file"
+            git add "$file"
+          fi
+        done
+      ''}";
+      language = "system";
+      stages = ["pre-commit"];
+      pass_filenames = false;
+    };
+
+    pre-push = {
+      enable = true;
+      entry = "cargo test -- --include-ignored";
+      stages = ["pre-push"];
+    };
   };
 
   enterShell = ''
